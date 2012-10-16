0efb71ce124a07397a463c6bfe97aff0daf70f4c
Sangwon Seo <sangwon.s@gmail.com>
2012-10-16 18:12:56 +0900

    o. Naver English Korean dictionary - Initial project version

---
+++ naverdic.js
@@ -0,0 +1,12 @@
+﻿function OnClick(info, tab) {
+    var url = "http://endic.naver.com/search.nhn?query=";
+    url += encodeURIComponent(info.selectionText);
+    url += "&x=0&y=0&kind=keyword";
+    
+    chrome.tabs.create({"index": tab.index+1, "url": url});
+}
+
+title = chrome.i18n.getMessage("NaverDicMenuTitle");
+context = "selection";
+chrome.contextMenus.create({"title": title, "contexts": [context],
+		"onclick": OnClick});
